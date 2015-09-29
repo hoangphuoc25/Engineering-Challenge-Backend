0dcb0e5e6b96e9eb3977d7f829540ee38db3aefc
Hoang Phuoc Truong <hoangphuoc25@gmail.com>
2015-09-29 21:57:40 +0800

    Revision 5, now every record has 17 nutrition fields

--- testxr.js
+++ testxr.js
@@ -17,6 +17,7 @@ function step1() {
 		  if (err) {
 		  	  reject(err);	
 		  } else {
+		  	// console.log('step1: ', obj);
 		  	resolve(obj);
 		  }
 		});	
@@ -26,12 +27,11 @@ function step1() {
 
 function step2(foodTypeUrl) {
 	return new Promise(function Promise(resolve, reject) {
-		xray(foodTypeUrl, '.food_description', [
-			'a@href'
-		])(function(err, obj) {
+		xray(foodTypeUrl, '.food_description', [{link: 'a@href'}])(function(err, obj) {
 			if(err){
 				reject(err);
 			} else{
+				// console.log('step2: ', obj);
 				resolve(obj);
 			}
 		});
@@ -41,14 +41,12 @@ function step2(foodTypeUrl) {
 step1()
 	.map(function map(objChild) {
 		return step2(objChild.link);
-	}, {concurrency : 500})
+	})
 	.map(function map(objChild) {
 		return Promise.map(objChild, function map(objChildChild) {
-			return step3(objChildChild);
-		}).catch(function error(err) {
-			console.log(err.stack);
+			return step3(objChildChild.link);
 		});
-	}, {concurrency : 500})
+	})
 	.then(function then(arr) {
 		console.log(arr);
 	})
@@ -59,20 +57,20 @@ step1()
 function step3(foodItemUrl) {
 	return new Promise(function Promise(resolve, reject) {
 		var a = foodItemUrl.split('/');
-		
+		// console.log('step3: ', foodItemUrl);
+		var foodId = a[a.length - 1];
 		if (!isNaN(a[a.length - 1])) {
-			console.log(foodItemUrl);
-			var foodId = parseInt(a[a.length - 1]);
 			xray(foodItemUrl, '#nutrition-facts tbody tr', [{
 				keys: ['td.col-1'],
 				values: ['td.col-2']
 			}])(function(err, obj) {
 				if (!err) {
-					console.log(foodId);
-					// var t = createNutritionObject(foodId, obj);
-					// resolve(t);
+					// console.log(foodId);
+					var t = createNutritionObject(foodId, obj);
+					
+					resolve(t);
 				} else {
-					// console.log(err.stack);
+					console.log(err.stack);
 				}
 			});
 		}
@@ -84,16 +82,31 @@ function createNutritionObject(foodId, obj) {
 		
 	  _.map(obj, function map(objValue) {
 	  	for (var i = 0; i < objValue.keys.length; i++) {
-	  		obj.nutritionalTable[objValue.keys[i]] = objValue.values[i];	
+	  		if (objValue.keys[i].trim())
+	  			obj.nutritionalTable[objValue.keys[i]] = objValue.values[i];
 	  	}
-	  	//console.log(obj.nutritionalTable);
-	  	delete obj._id;
-	  	console.log(obj.nutritionalTable);
-	  	db.fooditem.insert(obj.nutritionalTable);	  	
 	  });
-	  
-	  delete obj.nutritionalTable;
-	  // console.log(obj);
+
+  	
+  	var testObj = {foodId: foodId};
+  	console.log(foodId, ':', obj.nutritionalTable);
+  	
+  	db.fooditem.find(testObj, {}, function(err, rows) {
+  		if (!err) {
+  			console.log('no error ', foodId);
+  			console.log('insert: ', obj.nutritionalTable);
+  			if (foodId === '10147') {
+  				console.log('inspecting 10147: ', rows);
+  			}
+  			if (rows.length == 0) {
+					console.log('inserting');
+					db.fooditem.insert(obj.nutritionalTable);
+				}
+  		} else {
+  			console.log(err.stack);
+  		}
+  	});
+  	
 	  return obj;
 };
 
